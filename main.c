13f2aa3787e8256248e20b4ad3413bffbda3a4b3
RenanScarpin <renanparpinelliscarpin@gmail.com>
2023-10-12 10:39:37 -0300

    Added feof

--- main.c
+++ main.c
@@ -62,10 +62,11 @@ void Print(LISTA* lista){
 
 int main(void){
   char command[12];
-  LISTA* dicionario = lista_criar(10);
+  LISTA* dicionario = lista_criar(100);
 //  printf("Start\n"); // DEBUG
   while(1){
     scanf(" %s", command);
+    if(feof(stdin)) exit(0);
     if     (strcmp(command, "insercao") == 0)
       Insert(dicionario);
 
